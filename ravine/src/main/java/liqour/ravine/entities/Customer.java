9651fe89f0d6f5c6b4ba1cd479977b175a42d518
ravine13 <kuriaderek@gmail.com>
2024-07-01 23:05:22 +0300

    new: file changes

--- ravine/src/main/java/liqour/ravine/entities/Customer.java
+++ ravine/src/main/java/liqour/ravine/entities/Customer.java
@@ -14,21 +14,28 @@ public class Customer {
     @Id
     @Column(name="customer_id")
     private int customer_id;
+    
     @Column(name="fname")
     private String fname;
+    
     @Column(name="lname")
     private String lname;
+    
     @Column(name="email")
     private String email;
+    
     @Column(name="phone_number")
     private long phone_number;
+    
     @Column(name="image_url")
     private String image_url;
+    
     @Column(name="barber_id")
     private int barber_id;
+    
     @Column(name="service_id")
     private int service_id;
+    
     @Column(name="booking_id")
     private int booking_id;
-    
 }
--- ravine/src/main/java/liqour/ravine/repositories/BarberRepository.java
+++ ravine/src/main/java/liqour/ravine/repositories/BarberRepository.java
@@ -1,20 +1,14 @@
 package liqour.ravine.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import liqour.ravine.entities.Barber;
+import java.util.Optional;
 
 
-@Repository
-public interface BarberRepository extends JpaRepository<Barber,Integer> {
-    @Query("SELECT r FROM barber r WHERE r.id = :id")
-    Optional<Barber> findBarberById(@Param("id")Integer id);
-    
-
-    
+public interface BarberRepository extends JpaRepository<Barber, Integer> {
+    @Query("SELECT b FROM Barber b WHERE b.id = :id")
+    Optional<Barber> findBarberById(@Param("id") Integer id);
 }
+
